1ab00b1961929b3554f2b4566941b3a1e3a70dd5
edilson <edilsontor77@gmail.com>
2025-07-04 22:23:29 -0300

    initial setup: backend, frontend and electron with typescript

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+    res.send("Servidor backend rodando");
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
