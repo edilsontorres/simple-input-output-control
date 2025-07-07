81d7f7bd57341d397a8e0cb6db509c1594036b72
edilson <edilsontor77@gmail.com>
2025-07-06 22:24:36 -0300

    setting cors policy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import router from './routes/Registers';
+import cors from 'cors';
 import { initDb } from './db/Database';
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+app.use(cors());
 app.use('/registers', router);
 
 initDb().then(() => {
